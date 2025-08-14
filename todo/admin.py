eb82bc65944fdddd148ed39a0db0853979095fe1
Amith B V <amithrao0606@gmail.com>
2025-08-14 07:49:42 +0530

    Added search field to search the tasks by their name in Tasks table.

--- todo/admin.py
+++ todo/admin.py
@@ -4,6 +4,7 @@ from .models import Task
 # To display the column name on the tasks table on the admins screen :
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task',)
 
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
